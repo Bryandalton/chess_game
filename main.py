b9b8bc3ce2d6db35336e5f0ccd1471bf28b4904b
unknown <bryantdalton19@gmail.com>
2023-08-17 12:44:35 -0400

    check knight moves

--- main.py
+++ main.py
@@ -132,8 +132,8 @@ def check_options(pieces, locations, turn):
             moves_list= check_pawn(location, turn)
         elif piece == 'rook':
             moves_list = check_rook(location, turn)
-        # elif piece == 'knight':
-        #     moves_list = check_knight(location, turn)
+        elif piece == 'knight':
+            moves_list = check_knight(location, turn)
         # elif piece == 'bishop':
         #     moves_list = check_bishop(location, turn)
         # elif piece == 'king':
@@ -206,6 +206,57 @@ def check_pawn(position, color):
         if (position[0] - 1, position[1] - 1) in white_locations:
             moves_list.append((position[0] - 1, position[1] - 1))
     return moves_list
+
+#check knight moves
+def check_knight(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    #8 squares to check for knight two squares in one direction and one in another
+    targets= [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+    for i in range(8):
+        target = (position[0] + targets[i][0], position[1] + targets[i][1])
+        if target not in friends_list and 0<= target[0] <=7 and 0 <= target[1] <=7:
+            moves_list.append(target)
+    return moves_list
+
+
+#check bishop
+def check_bishop(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    return moves_list
+
+#check king
+def check_king(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    pass
+
+#check queen
+def check_queen(position, color):
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    pass
 
 #draw valid move for just selected piece
 def check_valid_moves():
